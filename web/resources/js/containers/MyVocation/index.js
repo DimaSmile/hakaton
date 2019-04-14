0d4517e2a24a7b07b811ed82ed73c262cb1ec788
Anna <ania.korotkovafd@gmail.com>
2019-04-14 16:28:15 +0300

    add vocations calendar

--- web/resources/js/containers/MyVocation/index.js
+++ web/resources/js/containers/MyVocation/index.js
@@ -1,23 +1,80 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import Grid from "@material-ui/core/Grid";
+import { dashboardAction } from "../../actions/Dashboard";
 import { styles } from "./style";
+import CalendarWithSelect from "../../components/CalendarWithSelect";
+import Modal from "../../components/Modal";
 
-class MyVocation extends Component {
+class MyVocation extends PureComponent {
+    state = {
+        showError: false,
+        errorText: ""
+    };
+    handleShowError = error => {
+        console.log("1");
+        this.setState({
+            showError: true,
+            errorText: error
+        });
+    };
+    componentDidMount() {
+        this.props.dashboardAction();
+    }
+    closeErrorModal = () => {
+        this.setState({
+            showError: false
+        });
+    };
     render() {
-        const { classes } = this.props;
+        const { classes, data, user } = this.props;
+        const { showError } = this.state;
+        const startWorking =
+            user && user.start_working ? user.start_working : false;
+        const vacation_days = data ? data.vacation_days : "";
         return (
-            <Card className={classes.card}>
-                <CardContent className={classes.cardContent}>
-                    <Typography paragraph>
-                        MY_VOCATION
-                    </Typography>
-                </CardContent>
-            </Card>
+            <Grid container spacing={16}>
+                <Grid item xs={10}>
+                    <Card>
+                        <CardContent>
+                            <CalendarWithSelect
+                                startWorking={startWorking}
+                                handleShowError={this.handleShowError}
+                            />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={2}>
+                    <Card className={classes.card}>
+                        <CardContent className={classes.cardContent}>
+                            <Typography className={classes.title} variant="h2">
+                                {vacation_days ? vacation_days : 0}
+                            </Typography>
+                            <Typography variant="h3" className={classes.title}>
+                                {vacation_days == 1 || vacation_days == -1
+                                    ? "day"
+                                    : "days"}
+                            </Typography>
+                            <Typography variant="h5" className={classes.title}>
+                                vacation
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Modal open={showError} handleClose={this.closeErrorModal}>
+                    <Card>
+                        <CardContent>
+                            <div>{this.state.errorText}</div>
+                        </CardContent>
+                    </Card>
+                </Modal>
+            </Grid>
         );
     }
 }
@@ -26,4 +83,22 @@ MyVocation.propTypes = {
     classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(MyVocation);
+const mapStateToProps = state => {
+    return {
+        data: state.dashboard.dashboardData,
+        user: state.user.user,
+        error: state.dashboard.usersErrors
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+    dashboardAction: bindActionCreators(dashboardAction, dispatch),
+    dispatch
+});
+
+const connector = connect(
+    mapStateToProps,
+    mapDispatchToProps
+);
+
+export default connector(withStyles(styles)(MyVocation));
--- web/resources/js/containers/MyVocation/style.js
+++ web/resources/js/containers/MyVocation/style.js
@@ -4,5 +4,23 @@ export const styles = theme => ({
     },
     cardContent: {
         borderRadius: 0
+    },
+    card: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "48%",
+        height: 200,
+        margin: "10px 0",
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        cursor: "all-scroll"
+    },
+    cardContent: {
+        borderRadius: 0,
+        textAlign: "center",
+        width: "100%"
+    },
+    title: {
+        color: "#ffffff"
     }
 });
